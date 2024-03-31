0982f24315b518fac06f9cec5efb610781a03b14
Filippo Visconti <filippo.visconti23@gmail.com>
2024-03-31 12:13:47 +0200

    feat: improve sketchybar config

--- sketchybar/plugins/disk.sh
+++ sketchybar/plugins/disk.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sketchybar --set $NAME label="$(df -H | grep -E '^(/dev/disk3s1).' | awk '{ printf ("%s\n", $5) }')"
+sketchybar --set $NAME label="$(df -H | grep -E '^(/dev/disk3s1s1).' | awk '{ printf ("%s\n", $5) }')"
